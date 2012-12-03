7e78c69f3f33f22a78b91521752d89c7d0576d7d
Kevin Ross <r0ssar00@gmail.com>
2012-12-03 16:29:55 -0500

    moved debug server startup code to the package wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,9 @@
+import sys
 from hvsi.wsgi import *
+if __name__ == '__main__':
+	if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+		from bottle import run
+		run(application, port=9055)
+	else:
+		from flup.server.fcgi import WSGIServer
+		WSGIServer(application).run()
