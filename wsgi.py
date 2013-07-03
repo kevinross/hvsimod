0654147f4082374a28fa057f16d5f0a6902e02f0
Kevin Ross <r0ssar00@gmail.com>
2013-07-03 02:35:34 -0400

    bind to all interfaces

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from hvsi.wsgi import *
 if __name__ == '__main__':
 	if len(sys.argv) > 1 and sys.argv[1] == 'debug':
 		from bottle import run
-		run(application, port=9055)
+		run(application, host='0.0.0.0', port=9055)
 	else:
 		from flup.server.fcgi import WSGIServer
 		WSGIServer(application).run()
